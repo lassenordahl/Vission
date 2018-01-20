2af60f5020dd803f3afa90cb24f0e683c1241b04
Lasse Nordahl <lasseanordahl@gmail.com>
2018-01-20 02:43:30 -0800

    semantic ui css framework installed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { 
+  Sidebar, 
+  Segment, 
+  Button, 
+  Menu, 
+  Image, 
+  Icon, 
+  Header ,
+  Input
+} from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button>Yeet</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
