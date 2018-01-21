a3975ebbe40abb83bcd9333adb52d93d13c6cbe9
BernardZhao <bernard.zhao.us@gmail.com>
2018-01-20 16:36:48 -0800

    oopsies

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const DB_CONFIG = {
+  apiKey: "AIzaSyCCsYSqa_CyfjVNib761b9CrdNc7X0bUGM",
+  authDomain: "vission-6d6fd.firebaseapp.com",
+  databaseURL: "https://vission-6d6fd.firebaseio.com",
+  projectId: "vission-6d6fd",
+  storageBucket: "vission-6d6fd.appspot.com",
+  messagingSenderId: "594035377988"
+};
