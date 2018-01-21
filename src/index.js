863e1e2f8da9cbb2c46c0f8550fee9eb5209d058
Zachary Zulanas <zzulanas@gmail.com>
2018-01-21 01:37:53 -0800

    bust a nut over there

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
-
 import firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
